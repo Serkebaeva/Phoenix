76ec2348d80f1b940c29dcef533ffa7869c72868
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2022-10-25 21:10:18 +0200

    assignment 3 - 1st part

---
+++ задание-3.js
@@ -0,0 +1,17 @@
+//Интернет-магазин занимается продажей различных сувениров и безделушек. Каждый сувенир весит 75 г, а безделушка – 112 г. Напишите программу, запрашивающую у пользователя количество тех и других покупок, после чего выведите на экран общий вес посылки.
+
+let souvenirsWeight = 75;
+let accessoriesWeight = 112;
+
+let quantityOfSouvenirs = Number(prompt("Введите колличество сувениров", "5"));
+let quantityOfAccessories = Number(
+  prompt("Введите колличество безделушек", "2")
+);
+
+let totalWeightOfSouvenirs = souvenirsWeight * quantityOfSouvenirs;
+let totalWeightOfAccessories = accessoriesWeight * quantityOfAccessories;
+let totalParselWeight = totalWeightOfAccessories + totalWeightOfSouvenirs;
+
+console.log(
+  `Общий вес сувениров - ${totalWeightOfSouvenirs}, Общий вес безделушек - ${totalWeightOfAccessories}, Общий вес посылки - ${totalParselWeight}`
+);
